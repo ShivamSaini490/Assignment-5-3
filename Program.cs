afa11ec7dea4dcbe47d13e721687e5f0206cce16
ShivamSaini490 <126521640+ShivamSaini490@users.noreply.github.com>
2023-12-04 14:50:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        string input = "shivam";
+
+        try
+        {
+            int result = int.Parse(input);
+            Console.WriteLine("Parsed value: " + result);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        Console.WriteLine("Program continues...");
+    }
+}
